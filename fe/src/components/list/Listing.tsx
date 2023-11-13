1d0e0e20cb2c334a3abcbc5b49adc07054ff7bc6
Avraham HAMU <avraham.hamu@beehero.io>
2023-11-13 10:58:52 +0200

    add css

--- fe/src/components/list/Listing.tsx
+++ fe/src/components/list/Listing.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
 import { People } from '../../types/common .types';
 import { Card } from '../card/Card';
+import styles from './listing.module.css';
 
 interface ListingProps {
     list: People[];
 }
+//         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
 
 export const Listing: FC<ListingProps> = ({ list }) => {
     return (
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <div className={styles.flexContainer}>
             {list?.map((item, index) => (
-                <Card {...item} key={`${item?.name}${index}`}  />
+                <div className={styles.flexItem}>
+                    <Card {...item} key={`${item?.name}${index}`} />
+                </div>
             ))}
         </div>
     );
